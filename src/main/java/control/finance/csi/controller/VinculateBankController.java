2cf25e15805495e1a3f4a51e1f24c498787ec9ca
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-05-27 09:57:53 -0300

    feat: Criando rota post para vincular banco ao usuário

--- src/main/java/control/finance/csi/controller/VinculateBankController.java
+++ src/main/java/control/finance/csi/controller/VinculateBankController.java
@@ -9,12 +9,25 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/vinculate-bank.jsp")
+@WebServlet("/vinculate-bank")
 public class VinculateBankController extends HttpServlet {
 
     @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String cpf = req.getParameter("cpf");
+        String bank = req.getParameter("bank");
+        String nameAccount = req.getParameter("name");
+        String initialBalance = req.getParameter("initialBalance");
+
+        System.out.println("Cpf: " + cpf);
+        System.out.println("Bank: " + bank);
+        System.out.println("Name Account: " + nameAccount);
+        System.out.println("Initial Balance: " + initialBalance);
+    }
+
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher rd = req.getRequestDispatcher("WEB-INF/views/vinculate-bank.jsp");
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/vinculate-bank.jsp");
         rd.forward(req, resp);
     }
 }
--- src/main/webapp/WEB-INF/views/vinculate-bank.jsp
+++ src/main/webapp/WEB-INF/views/vinculate-bank.jsp
@@ -10,7 +10,7 @@
     <c:if test="${not empty user}">
         ${user.getName()}
     </c:if>
-    <form>
+    <form method="post" action="/finance-control/vinculate-bank">
         <h2>Bancos disponiveis:</h2>
         <label for="bank">Bancos:</label>
         <select name="bank" id="bank" required>
@@ -23,8 +23,10 @@
         </select>
         <label for="name">Nome da Conta: </label>
         <input type="text" name="name" id="name" placeholder="Ex: Conta Corrente Itaú" required>
-        <label for="initial_balance">Valor Inicial: </label>
-        <input type="number" name="initial_balance" id="initial_balance" min="0" placeholder="Ex: 1000" required>
+        <label for="initialBalance">Valor Inicial: </label>
+        <input type="number" name="initialBalance" id="initialBalance" min="0" placeholder="Ex: 1000" required>
+        <input type="hidden" name="cpf" value="${user.getCpf()}">
+        <button type="submit">Enviar</button>
     </form>
 </body>
 </html>
